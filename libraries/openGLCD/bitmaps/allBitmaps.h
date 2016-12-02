223e7a489651305637896fabcb42400fc5b31582
ShailJoshi <shailjoshi007@gmail.com>
2016-12-02 19:26:03 +0530

    libraries added

---
+++ libraries/openGLCD/bitmaps/allBitmaps.h
@@ -0,0 +1,18 @@
+/* allBitmaps.h bitmap header for openGLCD library */
+/* This file is created automatically by the glcdMakeBitmap utility */
+/* Any edits to this file will be lost when glcdMakeBitmap is next run */
+
+#include "ArduinoIcon.h"
+#include "ArduinoIcon32.h"
+#include "ArduinoIcon32x32.h"
+#include "ArduinoIcon64x32.h"
+#include "ArduinoIcon64x64.h"
+#include "ArduinoIcon96x32.h"
+#include "BillsHead.h"
+#include "BillsHead_xbm.h"
+#include "car914_xbm.h"
+#include "kartlogo.h"
+#include "openGLCD128x64.h"
+#include "openGLCD64x32.h"
+#include "openGLCDh128x64.h"
+#include "tklogo.h"
